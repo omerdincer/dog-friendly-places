39628011a87f98f9a58baa0942d95e02a91f95b6
omerdincer <omerdincer90@hotmail.com>
2024-09-30 11:50:01 +0300

    added header and side bar to locationdetail

--- pup-cities-bangkok/src/pages/LocationDetail.js
+++ pup-cities-bangkok/src/pages/LocationDetail.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { db } from "../firebase"; // Firestore instance
 import { doc, getDoc } from "firebase/firestore";
+import Header from '../components/Header'; // Import Header component
 
 const LocationDetail = () => {
   const { id } = useParams(); // Get the ID from the URL
@@ -27,22 +28,30 @@ const LocationDetail = () => {
   }
 
   return (
-    <div className="p-6 bg-gray-100 shadow-lg rounded">
-      <h1 className="text-4xl font-bold mb-4">{location.name}</h1>
-      <img
-        src={location.imageURL || "../components/materials/default.png"}
-        alt={location.name}
-        className="w-full h-64 object-cover rounded mb-4"
-      />
-      <p className="text-lg mb-4">Type: {location.type}</p>
-      <p className="text-lg mb-4">Neighborhood: {location.neighborhood}</p>
+    <div>
+      {/* Add the Header at the top */}
+      <Header />
 
-      {/* Display the Google Maps iframe */}
-      {location.googleMapsLink && (
-        <div
-          dangerouslySetInnerHTML={{ __html: location.googleMapsLink }}
-        />
-      )}
+      {/* Main content container */}
+      <div className="container mx-auto p-4">
+        <div className="p-6 bg-gray-100 shadow-lg rounded">
+          <h1 className="text-4xl font-bold mb-4">{location.name}</h1>
+          <img
+            src={location.imageURL || "../components/materials/default.png"}
+            alt={location.name}
+            className="w-full h-64 object-cover rounded mb-4"
+          />
+          <p className="text-lg mb-4">Type: {location.type}</p>
+          <p className="text-lg mb-4">Neighborhood: {location.neighborhood}</p>
+
+          {/* Display the Google Maps iframe */}
+          {location.googleMapsLink && (
+            <div
+              dangerouslySetInnerHTML={{ __html: location.googleMapsLink }}
+            />
+          )}
+        </div>
+      </div>
     </div>
   );
 };
